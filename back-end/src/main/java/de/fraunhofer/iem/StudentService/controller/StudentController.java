a8509b8eac98b5dd4c31c9a34f44cf8427c579ee
Prakhar Rathi <prakhar@mail.uni-paderborn.de>
2020-12-05 14:06:39 +0100

    Add new student --> in REST

--- back-end/src/main/java/de/fraunhofer/iem/StudentService/controller/StudentController.java
+++ back-end/src/main/java/de/fraunhofer/iem/StudentService/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,7 +40,7 @@ public class StudentController {
    */ 
     //Add Student REST API
     @PostMapping("students")
-    public Student addStudent(Student student) {
-    	return student;
+    public Student createStudent(@RequestBody Student student) {
+    	return studentRecords.save(student);
     }
 }
