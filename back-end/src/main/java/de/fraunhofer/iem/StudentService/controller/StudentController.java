374b3a7d2ff9cfa6b835bb87d7b8a576bfdbed67
Prakhar Rathi <prakhar@mail.uni-paderborn.de>
2020-12-05 15:07:46 +0100

    Search student by Matriculation Number -- In REST API

--- back-end/src/main/java/de/fraunhofer/iem/StudentService/controller/StudentController.java
+++ back-end/src/main/java/de/fraunhofer/iem/StudentService/controller/StudentController.java
@@ -3,13 +3,16 @@ package de.fraunhofer.iem.StudentService.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import de.fraunhofer.iem.StudentService.exception.ResourceNotFoundException;
 import de.fraunhofer.iem.StudentService.model.Student;
 import de.fraunhofer.iem.StudentService.service.StudentRecords;
 import de.fraunhofer.iem.StudentService.service.StudentService;
@@ -42,5 +45,13 @@ public class StudentController {
     @PostMapping("students")
     public Student createStudent(@RequestBody Student student) {
     	return studentRecords.save(student);
+    }
+    
+    //get Student by Matriculation Number (ID)
+    @GetMapping("/students/{matriculationNumber}")
+    public ResponseEntity<Student> getStudentByID(@PathVariable Long matriculationNumber) {
+    	Student student = studentRecords.findById(matriculationNumber)
+    			.orElseThrow(() -> new ResourceNotFoundException("No student with the matriculation number: "+ matriculationNumber));
+		return ResponseEntity.ok(student);
     }
 }
