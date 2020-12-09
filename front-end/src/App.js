5173089f664199221c03805268da9b65cf04bb4e
Pegahmhz <75580449+Pegahmhz@users.noreply.github.com>
2020-12-09 14:52:37 +0100

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -62,4 +62,53 @@ class App extends Component {
   }
 }
 
+class UpdateDialog extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		const updatedStudent = {};
+		this.props.attributes.forEach(attribute => {
+			updated Student[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
+		});
+		this.props.onUpdate(this.props.student, updated Student);
+		window.location = "#";
+	}
+
+	render() {
+		const inputs = this.props.attributes.map(attribute =>
+			<p key={this.props.student.entity[attribute]}>
+				<input type="text" placeholder={attribute}
+					   defaultValue={this.props.student.entity[attribute]}
+					   ref={attribute} className="field"/>
+			</p>
+		);
+
+		const dialogId = "updateStudent-" + this.props.student.entity._links.self.href;
+
+		return (
+			<div key={this.props.student.entity._links.self.href}>
+				<a href={"#" + dialogId}>Update</a>
+				<div id={dialogId} className="modalDialog">
+					<div>
+						<a href="#" title="Close" className="close">X</a>
+
+						<h2>Update a student</h2>
+
+						<form>
+							{inputs}
+							<button onClick={this.handleSubmit}>Update</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		)
+	}
+
+};
+
 export default App;
